ebcfd018c1c0163d9ed1a8047e6bc71f66338598
eduardo rivas <r.eduardo92@gmail.com>
2019-08-14 11:28:05 -0500

    navFixed

--- src/componets/layout/NavBar/Navbar.jsx
+++ src/componets/layout/NavBar/Navbar.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import logo from '../../../img/vslogo.png';
 import styled from 'styled-components';
 
 const Styled = styled.header`
-  position: ${props => props.hideScroll};
-  /* background-color: transparent; */
+  position: fixed;
   top: 0;
   left: 0;
   right: 0;
@@ -17,7 +16,8 @@ const Styled = styled.header`
     justify-content: center;
     align-items: center;
     padding-top: 1.5em;
-    background-color: ${props => props.navBc};
+    background-color: #21212140;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 
     .nav-link {
       color: #a7a7a7;
@@ -50,42 +50,20 @@ const Styled = styled.header`
   }
 `;
 
-const NavbarCustom = () => {
-  const [scrolled, setScrolled] = useState(false);
-  const navRef = useRef();
-
-  useEffect(
-    () =>
-      window.addEventListener('scroll', () => {
-        if (window.scrollY > navRef.current.offsetHeight) {
-          document.body.style.paddingTop = `${navRef.current.offsetHeight}px`;
-          setScrolled(true);
-        } else {
-          document.body.style.paddingTop = 0;
-          setScrolled(false);
-        }
-      }),
-    []
-  );
-
-  return (
-    <Styled
-      hideScroll={scrolled ? 'fixed' : 'absolute'}
-      navBc={scrolled ? '#212121c2' : ''}
-    >
-      <nav ref={navRef} className='customNav'>
-        <NavLink className='nav-link' to='/home'>
-          Home
-        </NavLink>
-        <Link className='logo' to='/home'>
-          <img src={logo} alt='Popcorn Fun' />
-        </Link>
-        <NavLink className='nav-link' to='/battle'>
-          Battle
-        </NavLink>
-      </nav>
-    </Styled>
-  );
-};
+const NavbarCustom = () => (
+  <Styled>
+    <nav className='customNav'>
+      <NavLink className='nav-link' to='/home'>
+        Home
+      </NavLink>
+      <Link className='logo' to='/home'>
+        <img src={logo} alt='Popcorn Fun' />
+      </Link>
+      <NavLink className='nav-link' to='/battle'>
+        Battle
+      </NavLink>
+    </nav>
+  </Styled>
+);
 
 export default NavbarCustom;
